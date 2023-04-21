267b98691a06c06ee1ddee84a681c7d42310bbb5
Vitor Da Silva <vitor.pintodasilva@heig-vd.ch>
2023-04-21 15:59:23 +0200

    changements majeurs + extraits csv

--- src/js/code.js
+++ src/js/code.js
@@ -1,21 +1,35 @@
+// importation de la librairie D3
 import * as d3 from 'd3';
+
 /**
  * Affichage des 6 cartes avec les données du CSV
  */
-function copyTemplate() {
+function displayCards() {
+    // copie du template
     const node = document.querySelector('#template').cloneNode(true);
 
+    // récupération du container
     const container = document.querySelector('#container');
 
+    /**
+     * Récupération des données + affichage dans le HTML
+     */
     d3.csv("../data/data_compress.csv").then(function(data) {
         data.forEach(function(d) {
-            console.log(d);
+            // affichage du totale de vitimes
+            node.content.querySelector('.victim-number').textContent = Number(d.victims1) + Number(d.victims2) + Number(d.victims3) + Number(d.victims4) + Number(d.victims5);
+
+            // affichage de la photo du personnage
+            node.content.querySelector('.cara-img img').src = `assets/personnages/${d.name}.png`;
+
+            // affichage du nom du personnage
+            node.content.querySelector('.cara-name').textContent = d.name;
+
+            // injection du code HTML dans le container
+            container.insertAdjacentHTML('afterbegin', node.innerHTML);
         });
     });
-
-    for(let i = 0; i < 6; i++) {
-        container.insertAdjacentHTML('afterbegin', node.innerHTML);
-    }
 }
 
-copyTemplate();
+// appel de la fonction
+displayCards();
